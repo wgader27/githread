4304a0b576731e3933df455d12721e2ce79ae997
Wgader27 <wahel.gader27@gmail.com>
2026-01-30 07:03:43 +0100

    Rectification erreur deploiement

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,13 +1,13 @@
 import { getAuthSession } from "@/lib/auth"
 
 export default async function UserPage({
-    params:{
-        userId: string
-    }
+    params,
+}: {
+    params: { userId: string }
 }) {
     const session = await getAuthSession();
-    const user = await getUserProfile(params.userId)
     return (
-        <div>UserPage</div>
-  )
+        <div>UserPage: {params.userId}</div>
+    )
 }
+
